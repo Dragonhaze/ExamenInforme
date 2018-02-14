76a28abf78965e81a6567af9e009d390eb6e3d47
Dragonhaze <duncanporter@hotmail.es>
2018-02-14 09:25:23 +0100

    Añadido el proyecto

---
+++ Examen3TrimestreDuncan/DBConnection.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace Examen3TrimestreDuncan
+{
+
+    public class DbConnection
+    {
+        private DbConnection()
+        {
+        }
+
+        private string databaseName = string.Empty;
+        public string DatabaseName
+        {
+            get { return databaseName; }
+            set { databaseName = value; }
+        }
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static DbConnection _instance = null;
+        public static DbConnection Instance()
+        {
+            if (_instance == null)
+                _instance = new DbConnection();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            if (Connection == null)
+            {
+                if (String.IsNullOrEmpty(databaseName))
+                    return false;
+                string connstring = string.Format("Server=localhost; database={0}; UID=root; password=1234", databaseName);
+                connection = new MySqlConnection(connstring);
+                connection.Open();
+            }
+
+            return true;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
+
---
+++ Examen3TrimestreDuncan/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace Examen3TrimestreDuncan
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, RoutedEventArgs e)
+        {
+            var dbCon = DbConnection.Instance();
+            dbCon.DatabaseName = "pajareria";
+
+            if (dbCon.IsConnect())
+            {
+               
+                string query = "USE EMPRESA; SELECT cod_emple, nom_emple, edad, nom_dep  FROM empleados   INNER JOIN departamentos dept      ON departamento=Dept.id_depart; ";
+                
+
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                
+
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                
+
+                
+                while (rdr.Read())
+                {
+                    
+                    texto.Text += "Codigo empleado : " + rdr[0].ToString() + '\n' +
+                                  "Nombre : " + rdr[1].ToString() + '\n' +
+                                  "Edad : " + rdr[2].ToString() + '\n' +
+                                  "Departamento : " + rdr[3].ToString() + '\n' + '\n';
+
+
+                }
+                rdr.Close();
+
+            }
+        }
+
+        private void btnInf_Click(object sender, RoutedEventArgs e)
+        {
+            Informe informe= new Informe();
+
+            informe.ShowDialog();
+        }
+    }
+}
